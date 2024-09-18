9015142fff8f30eb441a798d185dc7cbcd02fca3
hrid1 <hridoycse1@gmail.com>
2024-09-19 01:16:13 +0600

    first login page done

---
+++ js/login.js
@@ -0,0 +1,21 @@
+console.log("Login info");
+
+const loginBtn = document.querySelector("#login-btn");
+
+loginBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+ 
+
+  const phoneNumber = document.getElementById("phone-num").value;
+  const pin = document.getElementById("pin-num").value;
+
+  console.log(phoneNumber, pin);
+
+  if (phoneNumber == 5 && pin == 1234) {
+    console.log("welcome");
+    window.location.href = "home.html"
+  }
+  else{
+    alert("Wrong Credinsial !!")
+  }
+});
