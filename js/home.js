cd51d58db207f88620c369d32ba66b7136f5b84f
hrid1 <hridoycse1@gmail.com>
2024-09-20 20:00:59 +0600

    Update Latest Paymetn section

--- js/home.js
+++ js/home.js
@@ -31,6 +31,7 @@ document.getElementById("cashout-btn").addEventListener("click", (e) => {
 
   if (pin === "123") {
     updateBalance(cashMoney, "withdraw");
+    updateTransactions("Withdraw", cashMoney);
   } else {
     alert("Wrong Credencial !!");
   }
@@ -43,39 +44,24 @@ document.getElementById("addmoney-btn").addEventListener("click", (e) => {
   const pin = getElement("addmoney-pin");
 
   const addMoney = parseInt(addmoneyAmount);
+  console.log("add money", addMoney);
 
   if (pin === "123") {
     updateBalance(addMoney, "deposite");
+    updateTransactions("Deposite", addMoney);
   } else {
     alert("wroing info");
   }
 });
 
 // ----------------switching category----------------------
-// document.getElementById("add-money-btn").addEventListener("click", (e) => {
-//   const options = document.getElementsByClassName("options");
-//   const optionsList = Array.from(options);
-//   const hideOptions = optionsList.filter(
-//     (option) => option.id !== "add-money-section"
-//   );
-//   console.log(optionsList);
-
-//   hideOptions.map((option) => option.classList.add("hidden"));
-//   document.getElementById("add-money-section").classList.remove("hidden");
-// });
-
-// const getElement2 = (elementId) => {
-
-//   };
 
 const switchOption = (btnId, sectionId) => {
   document.getElementById(btnId).addEventListener("click", (e) => {
     const options = document.getElementsByClassName("options");
     const optionsList = Array.from(options);
-    const hideOptions = optionsList.filter(
-      (option) => option.id !== sectionId
-    );
-    console.log(optionsList);
+    const hideOptions = optionsList.filter((option) => option.id !== sectionId);
+    // console.log(optionsList);
     hideOptions.map((option) => option.classList.add("hidden"));
     document.getElementById(sectionId).classList.remove("hidden");
   });
@@ -83,3 +69,28 @@ const switchOption = (btnId, sectionId) => {
 
 switchOption("add-money-btn", "add-money-section");
 switchOption("cash-out-btn", "cash-out-section");
+switchOption("transfer-btn", "latest-payment-section");
+
+// update Transactions
+
+const updateTransactions = (billName, amount) => {
+  const latestPaymentsParent = document.getElementById("latest-payments");
+  const item = document.createElement("div");
+
+  item.innerHTML = `
+    <div class="flex justify-between items-center bg px-4 py-4 rounded shadow shadow-gray-600">
+      <div class="flex gap-2.5 items-center">
+        <i class="fa-solid fa-money-bill-transfer text-xl"></i>
+        <div>
+          <h3>${billName}</h3>
+          <p>Total: $ <span class="font-semibold">${amount} </span>Tk </p>
+        </div>
+      </div>
+
+      <i class="fa-solid fa-ellipsis-vertical text-xl"></i>
+    </div>
+  `;
+  latestPaymentsParent.appendChild(item);
+};
+
+// updateTransactions("Shopping", 400);
--- js/login.js
+++ js/login.js
@@ -4,7 +4,6 @@ const loginBtn = document.querySelector("#login-btn");
 
 loginBtn.addEventListener("click", (event) => {
   event.preventDefault();
- 
 
   const phoneNumber = document.getElementById("phone-num").value;
   const pin = document.getElementById("pin-num").value;
@@ -13,9 +12,8 @@ loginBtn.addEventListener("click", (event) => {
 
   if (phoneNumber == 5 && pin == 1234) {
     console.log("welcome");
-    window.location.href = "home.html"
-  }
-  else{
-    alert("Wrong Credinsial !!")
+    window.location.href = "home.html";
+  } else {
+    alert("Wrong Credinsial !!");
   }
 });
